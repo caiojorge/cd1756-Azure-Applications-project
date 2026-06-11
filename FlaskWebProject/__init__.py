0b3bdea7bbe4917785c4dcb58bab09fec2276030
caiojorge <caio.jorge@gmail.com>
2026-06-11 19:45:03 -0300

    Update logging handler to output to stdout and adjust logger propagation settings

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -2,6 +2,7 @@
 The flask application package.
 """
 import logging
+import sys
 from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 from config import Config
@@ -13,13 +14,14 @@ app = Flask(__name__)
 app.config.from_object(Config)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
-handler = logging.StreamHandler()
+handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(logging.INFO)
 handler.setFormatter(logging.Formatter(
     '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
 ))
 app.logger.addHandler(handler)
 app.logger.setLevel(logging.INFO)
+app.logger.propagate = False
 app.logger.info('Application starting up')
 
 Session(app)
